76ca2b85d59abaa93d3da533b504eacca59140d5
Lifu Tao <lifutao07@gmail.com>
2020-10-08 15:33:52 -0400

    add routing to different pages

--- hma_frontend/src/App.js
+++ hma_frontend/src/App.js
@@ -2,11 +2,43 @@ import React from 'react';
 import './App.css';
 import Login from './Auth/Login.js'
 import SignUp from './Auth/SignUp.js'
+import MainPage from './MainPage/MainPage.js'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <SignUp/>
+    <Router>
+      <div>
+        <nav className="Home-Nav-Bar">
+          <ul className="Home-Nav-Bar-List">
+              <Link className="Nav-Link"
+                to="/">Main Page</Link>
+              <Link className="Nav-Link"
+              to="/signup">Sign Up</Link>
+              <Link 
+              className="Nav-Link"
+              to="/login">Login</Link>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+          <Route path="/">
+            <MainPage/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
     </div>
   );
 }
