bf21fe3aacf653852ae394db38f768715dfa6f51
Lifu Tao <lifutao07@gmail.com>
2020-10-08 14:34:48 -0400

    add Sign-Up Front End Screen

--- hma_frontend/src/App.js
+++ hma_frontend/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import Login from './Auth/Login.js'
+import SignUp from './Auth/SignUp.js'
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <SignUp/>
     </div>
   );
 }
